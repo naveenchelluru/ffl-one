c7a5e7d190df9ea0d70554be2f36749dc99f7903
naveenchelluru <chellurunaveen99@gmail.com>
2022-11-25 22:48:11 +0530

    After configuring new post page

--- app.js
+++ app.js
@@ -12,26 +12,34 @@ app.listen(3000,()=>{
     console.log("Server is running successfully");
 })
 
-const userSchema=mongoose.Schema({name:String,content:String,loaded:String});
+
+//data schema
+const userSchema=mongoose.Schema({name:{type:String,required:true},content:{type:String,required:true},loaded:String});
 
 const user=new mongoose.model("user",userSchema);
 const today=new Date();
-const app1=today.toDateString();
-const dog=new mongoose.model("man",userSchema);
+const days=today.toDateString();
 
+//feed back schema
+const feedSchema=mongoose.Schema({rating:{type:Number,required:true},opinion:String,loaded:String})
+
+const feedBack=new mongoose.model("feedback",feedSchema)
+// home page
 app.route("/").get((req,res)=>{
-  user.find((err,sol)=>{
-    res.render("home",{stream:sol})
-    console.log(sol.length);
-  })
+  try {
+    user.find((err,sol)=>{
+      res.render("home",{stream:sol,para:" "})
+
+    })
+  } catch (error) {
+    res.send(error)
+  }
  })
  .post((req,res)=>{
-  const name=req.body.title
-  const content=req.body.content
-  const man1=new dog({name:"krishna",content:"the true Teacher",loaded:app1})
-  man1.save()
+  
  })
 
+ //about page
  app.route("/about")
  .get((req,res)=>{
   res.render("about");
@@ -40,29 +48,60 @@ app.route("/").get((req,res)=>{
 
 })
 
+app.route("/newfeed")
+.get((req,res)=>{
+  res.render("newfeed")
+})
+.post((req,res)=>{
+  const title=req.body.title;
+  const story=req.body.story;
+ if(title)
+ {
+  if(story)
+  {
+    const newUser=new user({name:title,content:story,loaded:days})
+    newUser.save((err)=>{
+      if(err){
+        res.send(err)
+      }else{
+        res.redirect("/")
+      }
+    })
+  }
+ }
+})
+
+//Feedback route
 app.route("/feedback")
 .get((req,res)=>{
-res.send("feedback")
+res.render("feedback")
 })
 .post((req,res)=>{
-
-})
-
+  try {
+    const rating=req.body.rating
+    const opinion=req.body.description
+    console.log(rating,opinion);
+    if(rating){
+      if(opinion){
+        const feedbacks=new feedBack({rating:rating,opinion:opinion,loaded:days})
+        feedbacks.save((err)=>{
+          res.render("home",{stream:sol,para:"Thank You for submitting your feedback"})
+        })
+      }
+    }
+  } catch (error) {
+    res.send(error)
+  }}
+)
+//contact route
 app.route("/contact")
 .get((req,res)=>{
-res.send("contact")
+res.render("contact")
 })
 .post((req,res)=>{
-
+res.redirect("/")
 })
 
-app.route("/services")
-.get((req,res)=>{
-res.send("services")
-})
-.post((req,res)=>{
-
-})
 
 
 
--- date.js
+++ date.js
@@ -1,4 +1,4 @@
 const today=new Date();
-const app=today.toDateString();//to get date in wed nov 16 2022 in this format
+const app=today.getDate()-1;//to get date in wed nov 16 2022 in this format
 
-console.log(typeof(app));
+console.log(app);
